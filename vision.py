20c7022162f782684ad5c719939ef5df48b898b9
MattNRG <mattma@taltech.ee>
2026-07-19 22:24:27 +0300

    Imported and cleaned old vision code

---
+++ vision.py
@@ -0,0 +1,50 @@
+import os
+import socket
+import struct
+import pathlib
+
+if any(
+        not os.path.exists('proto/' + proto + '_pb2.py')
+        for proto in ('ssl_gc_referee_message', 'ssl_gc_common', 'ssl_gc_game_event', 'ssl_vision_wrapper')
+):
+    print("Compiling Protobuf files...")
+    import grpc_tools.protoc
+    grpc_tools.protoc.main([
+        'protoc',
+        '--python_out=.', '--pyi_out=.',
+        *[str(path) for path in pathlib.Path().rglob('proto/*.proto')]
+    ])
+
+from proto.ssl_vision_wrapper_pb2 import SSL_WrapperPacket
+
+def open_multicast_socket(ip: str, port: int) -> socket.socket:
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    sock.bind(('' if os.name == 'nt' else ip, port))
+
+    sock.setsockopt(
+        socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP,
+        struct.pack('4sl', socket.inet_aton(ip), socket.INADDR_ANY)
+    )
+    return sock
+
+
+visionSocket = open_multicast_socket('224.5.23.2', 10006)
+
+def getData():
+    data, address = visionSocket.recvfrom(1024)
+    print(data)
+    # print(data.decode('utf-8'))
+
+    packet = SSL_WrapperPacket()
+    packet.ParseFromString(data)
+
+    return packet
+
+
+print("getVision ready")
+while True:
+    data = getData()
+    print(data)
